76197ba0d153f90a70909a34b4ad67e958ba0d46
Sam Saravillo <7529759+samsaravillo@users.noreply.github.com>
2025-07-21 12:41:17 -0700

    ab#28279 Added dependency animation and updated styles

--- applications/Grants.ApplicantPortal/src/Grants.ApplicantPortal.Frontend/src/app/app.config.server.ts
+++ applications/Grants.ApplicantPortal/src/Grants.ApplicantPortal.Frontend/src/app/app.config.server.ts
@@ -1,11 +1,19 @@
 import { provideServerRendering, withRoutes } from '@angular/ssr';
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering(withRoutes(serverRoutes)), provideHttpClient(withInterceptorsFromDi())]
+  providers: [
+    provideServerRendering(withRoutes(serverRoutes)),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideNoopAnimations(),
+  ],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
