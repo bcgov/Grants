b11ce6f847a6348dfdfa29b9f53e0ec41107d81e
Sam Saravillo <7529759+samsaravillo@users.noreply.github.com>
2025-11-03 17:46:12 -0800

    Use environment variables for Keycloak authority
    - Refactored auth config to use environment.keycloakAuthority instead of hardcoded Keycloak authority URL
    - Added keycloakAuthority to both environment and environment.deploy files to support different environments

--- applications/Grants.ApplicantPortal/src/Grants.ApplicantPortal.Frontend/src/app/core/auth/auth.config.ts
+++ applications/Grants.ApplicantPortal/src/Grants.ApplicantPortal.Frontend/src/app/core/auth/auth.config.ts
@@ -1,8 +1,9 @@
 import { PassedInitialConfig, LogLevel } from 'angular-auth-oidc-client';
+import { environment } from '../../../environments/environment';
 
 export const authConfig: PassedInitialConfig = {
   config: {
-    authority: 'https://dev.loginproxy.gov.bc.ca/auth/realms/standard', // using dev for now
+    authority: environment.keycloakAuthority, // using environment variable
     redirectUrl: window.location.origin + '/auth/callback', // using dev for now using port 3000.It is available in CSS for now , Need to add 4000/ 4200 redirect
     postLogoutRedirectUri: window.location.origin + '/login',
     clientId: 'grants-portal-5361', //Resource
@@ -11,6 +12,6 @@ export const authConfig: PassedInitialConfig = {
     silentRenew: true,
     useRefreshToken: true,
     logLevel: LogLevel.Debug,
-    secureRoutes: ['https://localhost:7000/api'],
+    secureRoutes: [environment.apiUrl + '/api'],
   },
 };
