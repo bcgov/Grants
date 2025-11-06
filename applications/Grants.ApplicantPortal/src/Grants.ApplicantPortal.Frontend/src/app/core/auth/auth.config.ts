46100fdd74179d64cc83fd8e74047c5f7e840b0f
Todosichuk, Daryl JEDI:EX <Daryl.Todosichuk@gov.bc.ca>
2025-11-06 14:24:56 -0800

    AB#30736 Redirect URI for login not working

--- applications/Grants.ApplicantPortal/src/Grants.ApplicantPortal.Frontend/src/app/core/auth/auth.config.ts
+++ applications/Grants.ApplicantPortal/src/Grants.ApplicantPortal.Frontend/src/app/core/auth/auth.config.ts
@@ -3,15 +3,15 @@ import { environment } from '../../../environments/environment';
 
 export const authConfig: PassedInitialConfig = {
   config: {
-    authority: environment.keycloakAuthority, // using environment variable
-    redirectUrl: window.location.origin + '/auth/callback', // using dev for now using port 3000.It is available in CSS for now , Need to add 4000/ 4200 redirect
+    authority: `${environment.keycloak.authority}/realms/${environment.keycloak.realm}`,
+    redirectUrl: window.location.origin + '/auth/callback',
     postLogoutRedirectUri: window.location.origin + '/login',
-    clientId: 'grants-portal-5361', //Resource
-    scope: 'openid profile email', // Adjust scopes as needed
+    clientId: environment.keycloak.clientId,
+    scope: 'openid profile email',
     responseType: 'code',
     silentRenew: true,
     useRefreshToken: true,
     logLevel: LogLevel.Debug,
-    secureRoutes: [environment.apiUrl + '/api'],
+    secureRoutes: [environment.apiUrl],
   },
 };
--- applications/Grants.ApplicantPortal/src/Grants.ApplicantPortal.Frontend/src/environments/environment.deploy.ts
+++ applications/Grants.ApplicantPortal/src/Grants.ApplicantPortal.Frontend/src/environments/environment.deploy.ts
@@ -1,5 +1,10 @@
 export const environment = {
   production: true,
-  apiUrl: '/api', // This will be proxied by the frontend server
-  keycloakAuthority: '${KEYCLOAK__AUTHSERVERURL}', // Set by OpenShift
+  apiUrl: '/api', // This will be proxied by the frontend server or handled by OpenShift routing
+  keycloak: {
+    authority: '${KEYCLOAK__AUTHSERVERURL}',
+    realm: '${KEYCLOAK__REALM}',
+    clientId: '${KEYCLOAK__RESOURCE}',
+    clientSecret: '${KEYCLOAK__CREDENTIALS__SECRET}'
+  }
 };
