30fd1a0b0d11400122b3d0a846299842c068139a
Andre Goncalves <andre.goncalves@aot-technologies.com>
2026-03-26 12:09:17 -0700

    AB#32477 add matomo and codeQL fixes

--- applications/Grants.ApplicantPortal/src/Grants.ApplicantPortal.Frontend/src/environments/environment.development.ts
+++ applications/Grants.ApplicantPortal/src/Grants.ApplicantPortal.Frontend/src/environments/environment.development.ts
@@ -2,6 +2,11 @@ export const environment = {
   production: false,
   apiUrl: '/api', // Use proxy for Docker development
   orgbookApiUrl: 'https://orgbook.gov.bc.ca/api',
+  matomo: {
+    enabled: true,
+    url: '//dev-analytics-matomo.apps.silver.devops.gov.bc.ca/',
+    siteId: '2'
+  },
   keycloak: {
     authority: 'https://dev.loginproxy.gov.bc.ca/auth',
     realm: 'standard',
